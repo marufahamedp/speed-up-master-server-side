ba8bb8c9b7c123f8066ab8fe0899838f5f746353
marufahamedp <marufledp.info@gmail.com>
2021-11-13 18:20:58 +0600

    update10

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ async function run() {
     // })
 
 
-  
+
     // get orders
     app.get('/orders', async (req, res) => {
       const cursor = ordersCollection.find({});
@@ -171,7 +171,7 @@ async function run() {
     //       const cursor = ordersCollection.find(query);
     //   const order = await cursor.toArray();
     //   res.json(order);
-     
+
     // })
 
     // get single order
@@ -210,22 +210,22 @@ async function run() {
       res.json(result);
     })
 
-      //get reviews
-      app.get('/reviews', async (req, res) => {
-        const cursor = reviewsCollection.find({});
-        const review = await cursor.toArray()
-        res.send(review)
-      });
-  
-      // post reviews
-      app.post('/reviews', async (req, res) => {
-        const review = req.body;
-        const result = await reviewsCollection.insertOne(review);
-        console.log('hitting the review', req.body);
-        console.log('got user', result);
-        res.json(result);
-      })
-        // get single reviews
+    //get reviews
+    app.get('/reviews', async (req, res) => {
+      const cursor = reviewsCollection.find({});
+      const review = await cursor.toArray()
+      res.send(review)
+    });
+
+    // post reviews
+    app.post('/reviews', async (req, res) => {
+      const review = req.body;
+      const result = await reviewsCollection.insertOne(review);
+      console.log('hitting the review', req.body);
+      console.log('got user', result);
+      res.json(result);
+    })
+    // get single reviews
 
     app.get('/reviews/:id', async (req, res) => {
       const id = req.params.id;
@@ -249,24 +249,26 @@ async function run() {
       res.json(order);
     })
 
-    app.get('/users/:email',  async (req, res) => {
+    app.get('/users/:email', async (req, res) => {
       const email = req.params.email;
       const query = { email: email };
       const user = await usersCollection.findOne(query);
-      if (user.role) {
+
+      let isAdmin = false;
+      if (user?.role === 'admin') {
         isAdmin = true;
       }
       res.json({ admin: isAdmin });
     })
 
-    app.post('/users', verifyToken,  async (req, res) => {
+    app.post('/users', verifyToken, async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
       console.log(result);
       res.json(result);
     });
 
-    app.put('/users', verifyToken,  async (req, res) => {
+    app.put('/users', verifyToken, async (req, res) => {
       const user = req.body;
       const filter = { email: user.email };
       const options = { upsert: true };
@@ -292,10 +294,10 @@ async function run() {
       if (requester) {
         const requesterAccount = await usersCollection.findOne({ email: requester });
         if (requesterAccount.role === 'admin') {
-        const filter = { email: user.email };
-        const updateDoc = { $set: { role: 'admin' } };
-        const result = await usersCollection.updateOne(filter, updateDoc);
-        res.json(result);
+          const filter = { email: user.email };
+          const updateDoc = { $set: { role: 'admin' } };
+          const result = await usersCollection.updateOne(filter, updateDoc);
+          res.json(result);
         }
       }
       else {
